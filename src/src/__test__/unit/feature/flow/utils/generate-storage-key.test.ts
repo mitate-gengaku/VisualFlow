ae7b2fde5b99ae33fbd21804d6678ad6fd474af2
mitate-gengaku <mitategengaku@gmail.com>
2025-01-10 12:26:58 +0900

    test: generate-storage-key.test.tsファイルの作成 #23

---
+++ src/src/__test__/unit/feature/flow/utils/generate-storage-key.test.ts
@@ -0,0 +1,17 @@
+import { describe, expect, it, vi } from "vitest";
+import { generateStorageKey } from "@/features/flow/utils/generate-storage-key";
+
+describe('generateStorageKeyのテスト', () => {
+  it('関数を呼び出したとき、キー文字列が生成される', () => {
+    const key = generateStorageKey();
+
+    expect(key).not.toBeNull();
+    expect(typeof key).toBe("string");
+  });
+
+  it('キー文字列が生成されたとき、正しいフォーマットであること', () => {
+    const key = generateStorageKey();
+
+    expect(key).toMatch(/^save-data-\d{4}-\d{2}-\d{2}$/);
+  })
+});
