4281aa1f5e13384e051604566cfd1de2df2c8a78
mitate-gengaku <mitategengaku@gmail.com>
2025-02-17 23:47:42 +0900

    feat: 選択時に枠線に色がつく機能を追加 #34

--- src/src/features/flow/components/node/step-node.tsx
+++ src/src/features/flow/components/node/step-node.tsx
@@ -12,45 +12,50 @@ import { nodesAtom } from "@/features/flow/store/node";
 import { StepData } from "@/features/flow/types/step-data";
 import { cn } from "@/lib/utils";
 
-export const StepNode = memo(({ id, data, selected }: NodeProps<Node<StepData>>) => {
-  const nodes = useAtomValue(nodesAtom);
-  const connection = useAtomValue(connectionAtom);
-  const sourceNode = nodes.find((node) => node.id === connection?.source);
+export const StepNode = memo(
+  ({ id, data, selected }: NodeProps<Node<StepData>>) => {
+    const nodes = useAtomValue(nodesAtom);
+    const connection = useAtomValue(connectionAtom);
+    const sourceNode = nodes.find((node) => node.id === connection?.source);
 
-  return (
-    <>
-      <div className={cn(
-        "pb-4 text-sm border bg-white dark:bg-slate-950 w-64 max-w-64 flex flex-col gap-2 rounded shadow duration-200 transition-all",
-        selected && "border-blue-500 dark:border-blue-700 [box-shadow:0_0_20px_0_rgba(56,189,248,.4)]",
-        )}>
-        <div className="flex items-center gap-2 py-2 bg-slate-100 dark:bg-slate-900 px-3 rounded">
-          <BoxIcon className="size-3" />
-          <p className="text-xs">{data.name ? data.name : id}</p>
-        </div>
-        {data.run && (
-          <div className="px-3 text-[10px]">
-            <h3>run: </h3>
-            <Markdown
-              remarkPlugins={[remarkBreaks]}
-              className="leading-[1] text-gray-500"
-            >
-              {data.run}
-            </Markdown>
+    return (
+      <>
+        <div
+          className={cn(
+            "pb-4 text-sm border bg-white dark:bg-slate-950 w-64 max-w-64 flex flex-col gap-2 rounded shadow duration-200 transition-all",
+            selected &&
+              "border-blue-500 dark:border-blue-700 [box-shadow:0_0_20px_0_rgba(56,189,248,.4)]",
+          )}
+        >
+          <div className="flex items-center gap-2 py-2 bg-slate-100 dark:bg-slate-900 px-3 rounded-tr rounded-tl">
+            <BoxIcon className="size-3" />
+            <p className="text-xs">{data.name ? data.name : id}</p>
           </div>
-        )}
-      </div>
-      <CustomTargetHandle
-        type="target"
-        position={Position.Left}
-        connectionLimit={sourceNode?.type === "step" ? 1 : 20}
-      />
-      <CustomSourceHandle
-        type="source"
-        position={Position.Right}
-        connectionLimit={1}
-      />
-    </>
-  );
-});
+          {data.run && (
+            <div className="px-3 text-[10px]">
+              <h3>run: </h3>
+              <Markdown
+                remarkPlugins={[remarkBreaks]}
+                className="leading-[1] text-gray-500"
+              >
+                {data.run}
+              </Markdown>
+            </div>
+          )}
+        </div>
+        <CustomTargetHandle
+          type="target"
+          position={Position.Left}
+          connectionLimit={sourceNode?.type === "step" ? 1 : 20}
+        />
+        <CustomSourceHandle
+          type="source"
+          position={Position.Right}
+          connectionLimit={1}
+        />
+      </>
+    );
+  },
+);
 
 StepNode.displayName = "StepNode";
--- src/src/features/flow/components/node/workflow-node.tsx
+++ src/src/features/flow/components/node/workflow-node.tsx
@@ -6,37 +6,42 @@ import { CustomSourceHandle } from "@/features/flow/components/handle/custom-sou
 import { WorkflowData } from "@/features/flow/types/workflow-data";
 import { cn } from "@/lib/utils";
 
-export const WorkflowNode = memo(({ data, selected }: NodeProps<Node<WorkflowData>>) => {
-  return (
-    <>
-      <div className={cn(
-        "pb-4 text-sm border-2 bg-white dark:bg-slate-950 flex w-64 max-w-64 flex-col gap-2 rounded shadow duration-200 transition-all",
-        selected && "border-blue-500 dark:border-blue-700 [box-shadow:0_0_20px_0_rgba(56,189,248,.4)]",
-      )}>
-        <div className="flex items-center gap-2 py-2 bg-slate-100 dark:bg-slate-900 px-3 rounded">
-          <NetworkIcon className="size-3" />
-          <p className="text-xs">{data.name}</p>
-        </div>
-        {data.on && (
-          <div className="px-3 text-[10px]">
-            <h3>on: </h3>
-            <ul className="px-4">
-              {Object.keys(data.on).map((on) => (
-                <li className="list-disc text-gray-500 leading-3" key={on}>
-                  {on}
-                </li>
-              ))}
-            </ul>
+export const WorkflowNode = memo(
+  ({ data, selected }: NodeProps<Node<WorkflowData>>) => {
+    return (
+      <>
+        <div
+          className={cn(
+            "pb-4 text-sm border-2 bg-white dark:bg-slate-950 flex w-64 max-w-64 flex-col gap-2 rounded shadow duration-200 transition-all",
+            selected &&
+              "border-blue-500 dark:border-blue-700 [box-shadow:0_0_20px_0_rgba(56,189,248,.4)]",
+          )}
+        >
+          <div className="flex items-center gap-2 py-2 bg-slate-100 dark:bg-slate-900 px-3 rounded-tr rounded-tl">
+            <NetworkIcon className="size-3" />
+            <p className="text-xs">{data.name}</p>
           </div>
-        )}
-      </div>
-      <CustomSourceHandle
-        type="source"
-        position={Position.Right}
-        connectionLimit={20}
-      />
-    </>
-  );
-});
+          {data.on && (
+            <div className="px-3 text-[10px]">
+              <h3>on: </h3>
+              <ul className="px-4">
+                {Object.keys(data.on).map((on) => (
+                  <li className="list-disc text-gray-500 leading-3" key={on}>
+                    {on}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
+        <CustomSourceHandle
+          type="source"
+          position={Position.Right}
+          connectionLimit={20}
+        />
+      </>
+    );
+  },
+);
 
 WorkflowNode.displayName = "WorkflowNode";
